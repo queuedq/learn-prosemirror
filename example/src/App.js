d928008dde6f8463d06ece90f6c9364a934a961d
queued37 <queued37@gmail.com>
2018-09-14 17:14:13 +0900

    Change component name to MyEditor

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'learn-prosemirror'
+import MyEditor from 'learn-prosemirror'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <MyEditor text='Modern React component module' />
       </div>
     )
   }
