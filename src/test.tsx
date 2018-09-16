83434531f6135b2440e053a212ae9137f4837a1d
queued37 <queued37@gmail.com>
2018-09-16 22:39:52 +0900

    Add type checking
    `npm run typescript` should exit with exit code 0.

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
 import MyEditor from './'
+import { EditorState } from 'prosemirror-state'
 
 describe('MyEditor', () => {
   it('renders without crashing', () => {
-    shallow(<MyEditor />)
+    shallow(<MyEditor
+      editorState={new EditorState()}
+      onChange={(editorState: EditorState) => {editorState}}
+    />)
   })
 })
