6ec92d75da1c19ca289b4a30553f9c82c2cb1d85
queued37 <queued37@gmail.com>
2018-11-11 18:40:01 +0900

    Import CSS

--- src/MyEditor.tsx
+++ src/MyEditor.tsx
@@ -3,6 +3,8 @@ import { EditorState } from 'prosemirror-state';
 
 import ProseMirror from './ProseMirror';
 
+import styles from './styles.css';
+
 export interface Props {
   editorState: EditorState;
   onChange: (editorState: EditorState) => void;
@@ -14,7 +16,7 @@ export default class MyEditor extends React.Component {
   render() {
     const { editorState, onChange } = this.props;
     return (
-      <div>
+      <div className={styles.myEditorWrapper} >
         <ProseMirror
           editorState={editorState}
           onChange={onChange}
