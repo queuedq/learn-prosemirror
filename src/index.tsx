9a26661392d97f4412d738f277c3c18d17a5a6e9
queued37 <queued37@gmail.com>
2018-11-21 00:32:27 +0900

    WIP: bring code inside component

--- src/index.tsx
+++ src/index.tsx
@@ -4,33 +4,35 @@ import { schema } from 'prosemirror-schema-basic';
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
-const state = EditorState.create({ schema });
-const createView = (element: any) => {
-  const view = new EditorView(element, {
-    state,
-    dispatchTransaction(transaction: any) {
-      console.log(transaction);
-      console.log(
-        'Document size went from', transaction.before.content.size,
-        'to', transaction.doc.content.size,
-      );
-      const newState = view.state.apply(transaction);
-      view.updateState(newState);
-    },
-  });
-  return view;
-};
-
 export default class Editor extends React.Component {
   ref: React.RefObject<HTMLDivElement | null>;
+  editorState: EditorState;
 
   constructor(props: any) {
     super(props);
     this.ref = React.createRef();
+
+    this.editorState = EditorState.create({ schema });
   }
 
+  createView = (element: any) => {
+    const view = new EditorView(element, {
+      state: this.editorState,
+      dispatchTransaction(transaction: any) {
+        console.log(transaction);
+        console.log(
+          'Document size went from', transaction.before.content.size,
+          'to', transaction.doc.content.size,
+        );
+        const newState = view.state.apply(transaction);
+        view.updateState(newState);
+      },
+    });
+    return view;
+  };
+
   componentDidMount() {
-    createView(this.ref.current);
+    this.createView(this.ref.current);
   }
 
   render() {
