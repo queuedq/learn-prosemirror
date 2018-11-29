640074f3d2179d400ddf85038ad188d251fa436e
queued37 <queued37@gmail.com>
2018-11-30 01:17:27 +0900

    Add basic commands and keymaps

--- src/createState.ts
+++ src/createState.ts
@@ -1,8 +1,16 @@
 import { EditorState } from 'prosemirror-state';
+import { undo, redo, history } from 'prosemirror-history';
+import { keymap } from 'prosemirror-keymap';
+import { baseKeymap } from 'prosemirror-commands';
 import { textSchema } from './schema';
 
 export default () => {
   return EditorState.create({
     schema: textSchema,
+    plugins: [
+      history(),
+      keymap({'Mod-z': undo, 'Mod-Shift-z': redo}),
+      keymap(baseKeymap),
+    ],
   });
 };
